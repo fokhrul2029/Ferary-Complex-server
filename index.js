619f1cae71b193806347e6d94fc06823d75d852c
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-06-12 15:22:46 +0600

    added a route to show the coupons

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // MiddleWares
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zebesho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -23,11 +24,32 @@ async function run() {
     // await client.connect();
     // await client.db("admin").command({ ping: 1 });
 
+    const database = client.db("Ferary-Complex");
+    const slides = database.collection("slides");
+    const apartments = database.collection("apartments");
+    const coupons = database.collection("coupons");
+
     app.get("/", (req, res) => {
       res.send("Hello World");
     });
 
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.get("/slides", async (req, res) => {
+      const result = await slides.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/apartments", async (req, res) => {
+      const result = await apartments.find().toArray();
+      res.send(result);
+    });
+    app.get("/coupons", async (req, res) => {
+      const result = (await coupons.find().toArray()).reverse();
+      res.send(result);
+    });
+
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
